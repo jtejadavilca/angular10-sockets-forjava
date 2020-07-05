0129b88da1d2e66b2ab4206f87bfba98be3824b8
Jose Tejada <jtejadavilca@gmail.com>
2020-07-05 01:51:09 -0500

    Se implementa comunicación con server usando socketjs-client y stompjs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,42 @@
 import { Component } from '@angular/core';
-
+import * as Stomp from 'stompjs';
+import * as SockJS from 'sockjs-client';
+import $ from 'jquery';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-chat-frontend';
+
+  private serverUrl = 'http://localhost:8081/socket';
+  private title = 'WebSockets chat';
+  private stompClient;
+
+  constructor() {
+    this.initializeWebSocketConnection();
+  }
+
+  initializeWebSocketConnection() {
+
+    const ws = new SockJS(this.serverUrl);
+
+    this.stompClient = Stomp.over(ws);
+
+    const that = this;
+
+    this.stompClient.connect({}, (frame) => {
+      that.stompClient.subscribe('/chat', (message) => {
+        if (message.body) {
+            $('.chat').append('<div class=\'message\'>' + message.body + '</div>');
+        }
+      });
+    });
+  }
+
+  sendMessage(message) {
+
+    this.stompClient.send('/app/send/message' , {}, message);
+    $('#input').val('');
+  }
 }
